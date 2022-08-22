73254d98fcfd99778d8b68f75a4d4fa42d088f74
Kholwi <kholwi@outlook.com>
2022-08-22 11:21:13 +0200

    first commit

--- src/components/Menu/Container.js
+++ src/components/Menu/Container.js
@@ -1,20 +1,64 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 const Container = () => {
     return (
-        
-        <div class="font-bold text-xl mb-2 p-10">
-            <div class="box-border h-45 w-40 p-4 border-4 ...">
-                Since the result of using the box-sizing
+        <>
+            <div class="grid grid-cols-2 gap-6 max-w-[1100px] mx-auto px-2 p-10">
+                <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md     lg:max-w-md">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Tailwind CSS Card with Shadow</h2>
+                    <p className="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi
+                        autem accusamus odio minima fugiat, consectetur recusandae assumenda.</p>
+                    <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:outline-none">
+                        Read more
+                    </a>
+                </div>
+            
+                <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md     lg:max-w-md">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Tailwind CSS Card with Shadow</h2>
+                    <p className="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi
+                        autem accusamus odio minima fugiat, consectetur recusandae assumenda.</p>
+                    <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:outline-none">
+                        Read more
+                    </a>
+                </div>
 
-                Applying this to all elements is safe and wise:
-            </div>
-            <div class="box-border h-45 w-40 p-4 border-4 ...">
-                Since the result of using the box-sizing
+                <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md     lg:max-w-md">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Tailwind CSS Card with Shadow</h2>
+                    <p className="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi
+                        autem accusamus odio minima fugiat, consectetur recusandae assumenda.</p>
+                    <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:outline-none">
+                        Read more
+                    </a>
+                </div>
 
-                Applying this to all elements is safe and wise:
+                <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md     lg:max-w-md">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Tailwind CSS Card with Shadow</h2>
+                    <p className="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi
+                        autem accusamus odio minima fugiat, consectetur recusandae assumenda.</p>
+                    <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:outline-none">
+                        Read more
+                    </a>
+                </div>
+
+                <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md     lg:max-w-md">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Tailwind CSS Card with Shadow</h2>
+                    <p className="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi
+                        autem accusamus odio minima fugiat, consectetur recusandae assumenda.</p>
+                    <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:outline-none">
+                        Read more
+                    </a>
+                </div>
+                <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md     lg:max-w-md">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Tailwind CSS Card with Shadow</h2>
+                    <p className="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi
+                        autem accusamus odio minima fugiat, consectetur recusandae assumenda.</p>
+                    <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:outline-none">
+                        Read more
+                    </a>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
